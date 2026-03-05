e8b15a8b90fe88aba078e1ad615bfef8099f0fbb
Jamaludin Iqbal <jamaludin.iqbal@gmail.com>
2026-03-05 21:14:38 +1100

    adding files

---
+++ _content.html.php
@@ -0,0 +1,3 @@
+<h1>lorem Welcome <?= $username ?? "you" ?></h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, odio veniam. Numquam maxime quo esse nisi. Enim cum eos voluptatem!</p>
---
+++ included-content.php
@@ -0,0 +1,10 @@
+<?php
+echo "<p>BEFORE</p>";
+
+//variable
+$username = "Iqbal";
+
+//include another file..
+require_once "_content.html.php";
+
+echo "<p>AFTER</p>";
